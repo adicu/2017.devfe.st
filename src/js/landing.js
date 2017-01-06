3b5c1d378d9bb3ee598dc2379bdbcba0ab915b52
Evan Tarrh <evantarrh@gmail.com>
2017-01-06 01:48:18 -0500

    tryna get that schedule workin u kno

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,20 +1,112 @@
 $(document).ready(function() {
   var w = $(window);
 
+  if (w.scrollTop() > 0) {
+    $('nav').addClass('active-nav');
+  }
+
   w.scroll(function() {
     if (w.scrollTop() > 0) {
-      $('nav').addClass("active-nav");
+      $('nav').addClass('active-nav');
     }
     else {
-      $('nav').removeClass("active-nav");
+      $('nav').removeClass('active-nav');
     }
   });
+
+  var vh = window.innerHeight;
+  var lion = $('#lion');
+  var _height = 40;
+  var _left = 50;
+
+  moveLion = function() {
+    var curScroll = w.scrollTop();
+
+    if (curScroll < vh) {
+      _height = 100 * ((curScroll / vh) * 0.6 + 0.4);
+      _left = 100 * ((curScroll / vh) * 0.5 + 0.5);
+
+      lion.css({
+        'height': _height + '%',
+        'left': _left + '%'
+      });
+    }
+  }
+
+  function getDistFromTop(element) {
+    var position = 0;
+
+    while(element) {
+        position += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return position;
+  }
+
+  w.scroll(moveLion);
 
   $('#burger').click(function(e) {
     $(e.currentTarget).toggleClass("close");
     $('.d-f-wrapper ul').toggleClass("show");
     $('.d-f-wrapper').toggleClass("menu-open");
   });
+
+  // $('.d-f-wrapper a, .scrolly-link').each(function(i, el) {
+  //   $(el).click(function(e) {
+  //     console.log($(e.currentTarget.hash));
+  //     console.log(getDistFromTop($(e.currentTarget.hash)[0]));
+  //     // _y = $(e.currentTarget.hash)[0].getBoundingClientRect().top;
+  //     // $.scrollTo(_y, 50000);
+  //     $('html, body').animate({
+  //         scrollTop: getDistFromTop($(e.currentTarget.hash)[0])
+  //     }, 500);
+  //   })
+  // });
+
+  function _easeInOutQuad(x, t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+    return -c / 2 * ((--t) * (t - 2) - 1) + b;
+  }
+
+  /**
+   * Scroll to the top of the page, for scrollDuration ms, calling cb when done.
+   *
+   * @param {int} offset - the offset from the top of the document to scroll to
+   * @param {int} scrollDuration - how long the scroll should take, in ms
+   * @param {function} cb - callback to call when the scroll is complete
+   */
+  $.scrollTo = function(offset, scrollDuration, cb) {
+    cb = cb || function() {};
+    var startT = Date.now();
+    var startY = window.scrollY;
+    var distanceToTravel = offset - startY;
+    var percentComplete = 0;
+    var elapsed;
+    var scrollToPercent;
+    var scrollToY;
+
+    function step() {
+      setTimeout(function() {
+        if (percentComplete < 1) {
+          elapsed = Date.now() - startT;
+          percentComplete = elapsed / scrollDuration;
+          scrollToPercent = _easeInOutQuad(percentComplete, elapsed, 0, 1, scrollDuration);
+          scrollToY = scrollToPercent * distanceToTravel + startY;
+          window.scrollTo(0, scrollToY);
+          requestAnimationFrame(step);
+        } else {
+          window.scrollTo(0, offset);
+          cb();
+        }
+      }, 15);
+    }
+
+    step();
+  };
+
+
+
 
 
 
