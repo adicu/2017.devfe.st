cb02ee2a90453ff6f5f589320e846ca3c97dc17e
Evan Tarrh <evantarrh@gmail.com>
2017-01-05 21:22:53 -0500

    nav stylin

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
-	$('.d-f-wrapper').hover(
-		function() {
-			$('.d, .f').addClass("hovering");
-			console.log('1');
-		},
-		function() {
-			$('.d, .f').removeClass("hovering");
-			console.log('2');
-		}
-	);
+  var w = $(window);
+
+  w.scroll(function() {
+    if (w.scrollTop() > 0) {
+      $('nav').addClass("active-nav");
+    }
+    else {
+      $('nav').removeClass("active-nav");
+    }
+  });
+
+  $('#burger').click(function(e) {
+    $(e.currentTarget).toggleClass("close");
+    $('.d-f-wrapper ul').toggleClass("show");
+    $('.d-f-wrapper').toggleClass("menu-open");
+  });
+
+
+
+ //  var dfLock = false;
+ //  var transitionDuration = 800;
+
+	// $('.d-f').hover(
+	// 	function() {
+ //      if (!dfLock) {
+ //        dfLock = true;
+ //        $('.d, .f').addClass("hovering");
+ //      }
+ //    },
+ //    function() {
+ //      $('.d, .f').removeClass("hovering");
+ //      dfLock = false;
+ //    }
+ //  );
 });
